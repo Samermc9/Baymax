c264decb04ab6a8f622724adf8dbef3586bee90d
Albus <qusaih42@gmail.com>
2018-08-29 15:22:00 +0400

    Fixed the huge bug

--- src/main/java/com/samermc9/discordBot/EventHandler.java
+++ src/main/java/com/samermc9/discordBot/EventHandler.java
@@ -61,9 +61,8 @@ public class EventHandler extends ListenerAdapter {
         }
 
 
-        if (msg.startsWith(PREFIX)) {
             //HELP COMMAND (!help) - GENERAL OUTLINE OF COMMANDS
-            if (msg.startsWith(PREFIX + "help")) {
+            if (msg.startsWith(PREFIX +"help")) {
 
                 if (args.length > 1) {
                     event
@@ -83,7 +82,7 @@ public class EventHandler extends ListenerAdapter {
 
 
                 //HUG COMMAND (!hug user) - SENDS A HUG MESSAGE
-            } else if (msg.startsWith(PREFIX + "hug")) {
+            } else if (msg.startsWith(PREFIX +"hug")) {
 
                 if (args.length < 2) {
                     event
@@ -111,7 +110,7 @@ public class EventHandler extends ListenerAdapter {
 
 
                 //SLAP COMMAND (!slap user) - SENDS A SLAP GIF AND A MESSAGE
-            } else if (msg.startsWith(PREFIX + "slap")) {
+            } else if (msg.startsWith(PREFIX +"slap")) {
                 if (args.length < 2) {
                     event
                             .getChannel()
@@ -138,7 +137,7 @@ public class EventHandler extends ListenerAdapter {
 
 
                 //KILL COMMAND (!kill user) - SENDS A RANDOM KILL GIF AND MESSAGE
-            } else if (msg.startsWith("kill")) {
+            } else if (msg.startsWith(PREFIX +"kill")) {
                 if (args.length < 2) {
                     event
                             .getChannel()
@@ -163,7 +162,7 @@ public class EventHandler extends ListenerAdapter {
                             .queue();
                 }
 
-            } else if (msg.startsWith("info")) {
+            } else if (msg.startsWith(PREFIX +"info")) {
                 if (args.length > 2) {
                     event
                             .getChannel()
@@ -183,7 +182,7 @@ public class EventHandler extends ListenerAdapter {
 
 
             //MEMBERS COMMAND (!members) - CHECKS TOTAL AMOUNT OF MEMBERS IN GIVEN SERVER.
-            else if (msg.startsWith("members")) {
+            else if (msg.startsWith(PREFIX +"members")) {
 
                 if (args.length > 1) {
                     event
@@ -201,7 +200,7 @@ public class EventHandler extends ListenerAdapter {
 
 
             //DELETE COMMAND (!delete [amount]) - DELETES MESSAGES BASED ON AMOUNT INPUT, REQUIRES ADMIN
-            else if (msg.startsWith("delete")) {
+            else if (msg.startsWith(PREFIX +"delete")) {
                 if (args.length < 2) {
                     event
                             .getChannel()
@@ -221,7 +220,7 @@ public class EventHandler extends ListenerAdapter {
 
 
             //ONLINE COMMAND (!online) - CHECKS FOR MEMBERS THAT ARE IN ONLINE STATUS
-            else if (msg.startsWith("online")) {
+            else if (msg.startsWith(PREFIX +"online")) {
 
                 if (args.length > 1) {
                     event
@@ -251,7 +250,7 @@ public class EventHandler extends ListenerAdapter {
                 }
 
 
-            } else if (msg.startsWith("kick")) {
+            } else if (msg.startsWith(PREFIX +"kick")) {
                 if (args.length < 2) {
                     event
                             .getChannel()
@@ -274,7 +273,7 @@ public class EventHandler extends ListenerAdapter {
 
 
             //DICE COMMAND (!dice) - ROLLS A DICE WITH BOUNDS OF 1 - 6.
-            else if (msg.startsWith("dice")) {
+            else if (msg.startsWith(PREFIX +"dice")) {
 
                 if (args.length > 1) {
                     event
@@ -305,12 +304,14 @@ public class EventHandler extends ListenerAdapter {
 
 
             } else {
+
+            if(msg.startsWith(PREFIX)) {
                 event
                         .getChannel()
                         .sendMessage("**:x: Invalid command! Not available - contact @Samermc9 for more info**")
                         .queue();
+                }
             }
-        }
     }
 }
 
